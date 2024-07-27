f9b43646c6a821babdd69e268c961f40fc866bd6
Ryan Conk <ryangconk@gmail.com>
2024-07-27 11:02:47 -0600

    add more file structures

---
+++ styles/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle `
+    body {
+        font-family: 'Arial', sans-serif
+        margin: 0;
+        padding:0;
+        background-color: #f4f4f4;
+        }
+        a {
+        text-decoration: none;
+        color: inherit;
+        
+        
+        }
+`
+export default GlobalStyles;
