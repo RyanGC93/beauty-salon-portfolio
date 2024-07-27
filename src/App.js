85f19f2a21bfcc4b3181ab72c972f0b0896a26f8
Ryan Conk <ryangconk@gmail.com>
2024-07-27 11:11:24 -0600

    add app page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Team from './pages/Team';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <GlobalStyles />
+    <Header />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/services" component={Services} />
+      <Route path="/team" component={Team} />
+      <Route path="/gallery" component={Gallery} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
+    <Footer />
+  </Router>
+);
 
 export default App;
