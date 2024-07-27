f030776f23e261ea446a74f19c0a9d1d99002518
Ryan Conk <ryangconk@gmail.com>
2024-07-27 11:03:52 -0600

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const HeaderContainer = styled.header `
- background-color: #fff;
- pa
+const HeaderContainer = styled.header`
+  background-color: #fff;
+  padding: 1rem 2rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+const Logo = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #333;
+`;
 
+const Nav = styled.nav`
+  a {
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: #333;
+  }
+`;
 
+const Header = () => (
+  <HeaderContainer>
+    <Logo>Beauty Salon</Logo>
+    <Nav>
+      <Link to="/">Home</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/team">Team</Link>
+      <Link to="/gallery">Gallery</Link>
+      <Link to="/contact">Contact</Link>
+    </Nav>
+  </HeaderContainer>
+);
 
-
-
-
-
-
-
+export default Header;
 
 
 
